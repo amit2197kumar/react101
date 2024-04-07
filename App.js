d781dd304cee05c3e08d22cd5968572f3edbf2a0
Amit Kumar <kumam@adobe.com>
2024-04-07 14:28:49 +0530

    Parent and child elements in plain React

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
-const parent = React.createElement("div", { className: "parent" }, [
-  React.createElement("h1", { className: "heading" }, "Heading 1"),
-  React.createElement(
-    "h2",
-    { className: "subheading" },
-    "Sub-Heading of Heading 1"
-  ),
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", { className: "heading" }, "Heading 1"),
+    React.createElement(
+      "h2",
+      { className: "subheading" },
+      "Sub-Heading of Heading 1"
+    ),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", { className: "heading" }, "Heading 2"),
+    React.createElement(
+      "h2",
+      { className: "subheading" },
+      "Sub-Heading of Heading 2"
+    ),
+  ]),
 ]);
 
 ReactDOM.render(parent, document.getElementById("root"));
