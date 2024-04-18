c60906d763c8f5834d4599ed973bc8ee9596296f
Amit Kumar <kumam@adobe.com>
2024-04-18 23:35:15 +0530

    Added README notes

--- components/Header.js
+++ components/Header.js
@@ -1,19 +1,29 @@
 import { LOGO_IMAGE } from "../utils/constants";
+import { useState } from "react";
 
 const Header = () => {  
+
+const [authState, setAuthState] = useState("Sign-In");
+
   return (
     <nav className="nav-bar">
       <div>
         <img src={LOGO_IMAGE} alt="logo" className="logo" />
       </div>
 
-      <div>
+      <div className="nav-items">
         <ul className="nav-links">
           <li>Home</li>
           <li>About</li>
           <li>Contact</li>
           <li>Cart</li>
         </ul>
+
+        <button className="signIn-btn" onClick={
+          () => {
+            setAuthState(authState === "Sign-In" ? "Sign-Out" : "Sign-In");
+          }
+        }>{authState}</button>
       </div>
     </nav>
   );
